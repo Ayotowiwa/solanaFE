545ee2f06799a9ca705db2bcff4018258b2087ef
Nuel <christlightsied@gmail.com>
2025-07-22 17:24:29 +0100

    starting faucet builder

--- pages/faucet/starter.tsx
+++ pages/faucet/starter.tsx
@@ -1,7 +1,12 @@
-import BoilerPlate from '../../components/BoilerPlate';
-
-const Starter = () => (
-    <BoilerPlate />
-)
+import * as React from 'react';
+// throws notifications for user friendly error handling
+import { toast } from 'react-toastify';
+// imports methods for deriving data from the wallet's data store
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+// imports icons
+import { ExternalLinkIcon } from '@heroicons/react/outline';
+// library we use to interact with the solana json rpc api
+import * as web3 from '@solana/web3.js';
+const Starter = () => {}
 
 export default Starter;
